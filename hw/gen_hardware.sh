3c196eb6fc89248e4426040f9266fc9fab5ad1b7
fritz <fritz@localhost>
2011-03-17 15:24:23 -0500

    plpbot hardware commit

---
+++ hw/gen_hardware.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#generates the plpbot hardware from the reference PLP source and the plpbot patch
+#requires mercurial and the xilinx webpack with the xilinx settings sourced
+
+hg clone https://progressive-learning-platform.googlecode.com/hg plpbot_hw
+cd plpbot_hw
+patch -p1 ../plpbot.patch
+
+cd reference/plp-2.2/hw
+sh scripts/build_500k_linux.sh
+
+
