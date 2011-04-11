7f8bb12c9cf22ccc8af1e40b652c89262cd7e19f
wira <wira@Alaei>
2011-04-11 18:35:47 -0500

    wasd control checked in

---
+++ sw/PLPBotControl/src/plpbotcontrol/Global.java
@@ -0,0 +1,46 @@
+/*
+    Copyright 2011 Wira Mulia, David Fritz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package plpbotcontrol;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.Socket;
+import java.awt.Image;
+
+/**
+ *
+ * @author wira
+ */
+public class Global {
+    public static VideoFrame    videoFrame;
+    public static ControlFrame  controlFrame;
+    public static Socket        baseStation;
+    public static URL           streamLocator;
+    public static URLConnection streamConnection;
+    public static Image         streamFrame;
+    public static String        baseStationHost;
+    public static int           baseStationPort;
+
+    public static int           buffers = 10;
+
+    public static int           grabberRate = 1;
+    public static int           painterRate = 1;
+    public static int           senderRate = 50;
+
+    public static int           comboKeyOffset = 40;
+}
---
+++ sw/PLPBotControl/src/plpbotcontrol/Main.java
@@ -0,0 +1,54 @@
+/*
+    Copyright 2011 Wira Mulia, David Fritz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package plpbotcontrol;
+
+/**
+ *
+ * @author wira
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+
+        if(args.length != 1 && args.length != 3) {
+            System.err.println("usage: java -jar PLPBotControl [camera's IP address] [base station IP address] [port]");
+            System.err.println("OR to view camera only: java -jar PLPBotControl [camera's IP address]");
+
+            return;
+        }
+
+        Global.streamLocator = new java.net.URL("http://" + args[0] + "/axis-cgi/jpg/image.cgi");
+        if(args.length == 3) {
+            Global.baseStationHost = args[1];
+            Global.baseStationPort = Integer.parseInt(args[2]);
+            Global.controlFrame = new ControlFrame();
+            Global.controlFrame.setVisible(true);
+        }
+        Global.videoFrame = new VideoFrame();
+        Global.videoFrame.setVisible(true);
+
+        } catch(Exception e) {
+            System.err.println("main exception: " + e);
+        }
+    }
+
+}
